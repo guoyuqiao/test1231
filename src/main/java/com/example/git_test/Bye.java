7ead92f11442b126be78b12f68cd810d8111b277
guoyuqiao <1453321546@qq.com>
2021-01-01 16:23:18 +0800

    bye

---
+++ src/main/java/com/example/git_test/Bye.java
@@ -0,0 +1,19 @@
+package com.example.git_test;
+
+public class Bye {
+    public static void main(String[] args) {
+        System.out.println("bye,bye,bye,bye");
+        System.out.println("bye,bye,bye,bye");
+
+        System.out.println("bye,bye,bye,bye");
+
+        System.out.println("bye,bye,bye,bye");
+
+        System.out.println("bye,bye,bye,bye");
+        System.out.println("bye,bye,bye,bye");
+        System.out.println("bye,bye,bye,bye");
+        System.out.println("bye,bye,bye,bye");
+
+
+    }
+}
